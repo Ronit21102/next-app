8dad4e08f45c6ee3eb0f790ac5184cd986db7e41
Ronit Roushan <ronit21102@gmail.com>
2024-04-10 11:55:27 +0530

    10 videos done

---
+++ app/Components/Addtocart.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+const Addtocart = () => {
+  return (
+    <button onClick={()=>console.log('clicked')}>Add to cart</button>
+
+  )
+}
+
+export default Addtocart
---
+++ app/Components/ProductPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Addtocart from './Addtocart'
+
+const ProductPage = () => {
+  return (
+    <div>
+        <h1>This is server side code</h1>
+        <Addtocart/>
+    </div>
+  )
+}
+
+export default ProductPage
